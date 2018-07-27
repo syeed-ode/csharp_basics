4d3f83bb73d6d749eb46fd7c2bc36b23cd78b79e
Syeed Ode <sy_ode@yahoo.com>
2018-07-27 11:27:34 +0300

    MAC: Delegate evaluations from mac perspective

---
+++ delegate_evaluation/delegate_evaluation/ConvertsIntToString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace delegate_evaluation
+{
+    // ConvertsIntToString is a delegate type 
+    // that you’ve added to your project. Now 
+    // you can use it to declare variables. 
+    // This is just like how you can use a 
+    // class or interface as a type to define 
+    // variables.
+    delegate string ConvertsIntToString(int i);
+}
---
+++ delegate_evaluation/delegate_evaluation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace delegate_evaluation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // someMethod is a variable whose type is 'ConvertsIntToString'. 
+            // It’s a lot like a reference variable, except instead of 
+            // putting a label on an object on the heap you’re putting 
+            // a label on a method.
+
+            // You can set 'someMethod()' just like any other variable. 
+            // When you call it like a method, it calls whatever method 
+            // it happens to point to.
+
+            // NOTE: The 'HiThere' method is an argument to the 
+            // 'ConvertsIntToString' declaration. 
+            ConvertsIntToString someMethod = new ConvertsIntToString(HiThere);
+
+
+            // The 'someMethod' variable is pointing to the 'HiThere()' 
+            // method.
+
+            // When your program calls someMethod(5), it HiThere() and 
+            // passes it the argument 5.
+            string message = someMethod(5);
+
+            // This causes it to return the string value “Hi there! 
+            // #500” -— exactly as if it were called directly.
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+
+        // This method’s signature matches 
+        // ConvertsIntToString.
+        private static string HiThere(int i)
+        {
+            return "Hi there! #" + (i * 100);
+        }
+    }
+}
